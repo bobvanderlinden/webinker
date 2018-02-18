7f38307cfc94c88c2483956565c2c42b6f93bf60
FrozenCow <frozencow@gmail.com>
2018-02-18 16:05:56 +0100

    cleanup of unused code

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 const HeadlessChrome = require("simple-headless-chrome");
 const im = require('imagemagick-stream');
-const fs = require('fs')
-const net = require('net')
-const toArray = require('stream-to-array')
-const unique = require('array-unique')
 const Readable = require('stream').Readable;
 const express = require('express');
 
@@ -20,75 +16,12 @@ app.get('/', (req, res) => {
   res.status = 200;
   res.setHeader('Content-Type', 'image/png');
   const screenshot = getScreenshotStream(url);
-  // const screenshot = fs.createReadStream('test.png');
   const grayScreenshot = convertToGrayscale(screenshot)
-  // const grayScreenshot = fs.createReadStream('gray.raw')
-  // grayScreenshot.pipe(fs.createWriteStream('gray.raw'))
-  // let counter = 0
-  // grayScreenshot.on('data', data => {
-  //   counter += data.length
-  // })
-  // grayScreenshot.on('end', () => {
-  //   console.log('SENT', counter, 'bytes')
-  // })
-  // const ding = net.createConnection(12345, '192.168.1.25', () => {
-  //   ding.write('@')
-  //   grayScreenshot.pipe(ding)
-  // })
-  // ding.on('finish', () => {
-  //   console.log('ding end')
-  //   res.end()
-  // })
-  // const TwoBppScreenshot = convertTo2bpp(grayScreenshot)
-  // TwoBppScreenshot.pipe(fs.createWriteStream('test.raw'));
   const output = grayScreenshot.pipe(res)
   output.on('end', () => {
     console.log(`Finished streaming ${url}`)
   })
 })
-
-function convertTo2bpp(input) {
-  console.log('convertTo2bpp')
-  const output = new Readable();
-  output._read = () => {};
-  toArray(input, (err, buffers) => {
-    const arr = []
-    for(let buffer of buffers) {
-      arr.push(...buffer)
-    }
-    const uniqueBytes = {};
-    for(let byte of arr) {
-      uniqueBytes[byte] = true;
-    }
-    console.log(Object.keys(uniqueBytes))
-    const uniqueColors = unique(arr)
-    console.log('uniqueColors', uniqueColors.length)
-    console.assert(uniqueColors.length <= 4, 'Too many colors')
-    const sortedColors = uniqueColors.sort()
-    while (sortedColors.length < 4) {
-      sortedColors.push(0xff);
-    }
-    const outputArr = [];
-    for (let i = 0; i < arr.length; i += 4) {
-      const color1 = sortedColors.indexOf(arr[i]);
-      const color2 = sortedColors.indexOf(arr[i+1]);
-      const color3 = sortedColors.indexOf(arr[i+2]);
-      const color4 = sortedColors.indexOf(arr[i+3]);
-      const outputByte =
-        (color1 << 6) &
-        (color2 << 4) & 
-        (color3 << 2) &
-        (color4 << 0);
-      outputArr.push(outputByte);
-    }
-    const outputStr = String.fromCharCode.apply(null, outputArr);
-    console.log('outputArr', outputArr);
-    console.log('outputStr', outputStr);
-    output.push(outputStr);
-    output.push(null);
-  })
-  return output
-}
 
 function convertToGrayscale(stream) {
   const converter = im()
